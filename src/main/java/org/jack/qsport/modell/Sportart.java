ab5f071bf8b574834cae77c8e911fefa0dce3ed0
Claus Behl <behl.claus@jack-steinberger-gymnasium.de>
2020-05-18 18:37:59 +0200

    Enumeration Sportart zu normaler Klasse gemacht, um später neue Sportarten zu erlauben..

--- src/main/java/org/jack/qsport/modell/Sportart.java
+++ src/main/java/org/jack/qsport/modell/Sportart.java
@@ -1,24 +1,64 @@
 package org.jack.qsport.modell;
 
-public enum Sportart {
-    Unbekannt(Art.SONSTIGE), Basketball(Art.TEAM), Fußball(Art.TEAM), Volleyball(Art.TEAM), GymnastikTanz(Art.EINZEL), 
-    Schwimmen(Art.EINZEL), Leichtathletik(Art.EINZEL), Mountainbiking(Art.SONSTIGE), Badminton(Art.SONSTIGE);
-    
-    Art art;
-    
-    Sportart(Art a){
-        art = a;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Sportart implements Serializable {
+
+    public static Sportart Unbekannt = new Sportart("Unbekannt", 0, Art.SONSTIGE);
+    public static Sportart Basketball = new Sportart("Basketball", 1, Art.TEAM);
+    public static Sportart Fußball = new Sportart("Fußball", 2, Art.TEAM);
+    public static Sportart Volleyball = new Sportart("Volleyball", 3, Art.TEAM);
+    public static Sportart GymnastikTanz = new Sportart("GymnastikTanz", 3, Art.EINZEL);
+    public static Sportart Schwimmen = new Sportart("Schwimmen", 4, Art.EINZEL);
+    public static Sportart Leichtathletik = new Sportart("Leichtathletik", 5, Art.EINZEL);
+    public static Sportart Mountainbiking = new Sportart("Mountainbiking", 6, Art.SONSTIGE);
+    public static Sportart Badminton = new Sportart("Badminton", 7, Art.SONSTIGE);
+
+    private final int ordinal;
+    private final Art art;
+    private final String name;
+
+    private static final ArrayList<Sportart> SPORTARTEN = new ArrayList<>();
+
+    protected Sportart(String name, int ordinal, Art a) {
+        this.name = name;
+        this.ordinal = ordinal;
+        this.art = a;
+        SPORTARTEN.add(this);
     }
-    
-    public Art getArt(){
+
+    public static Sportart[] values() {
+        return SPORTARTEN.toArray(new Sportart[0]);
+    }
+
+    public static Sportart valueOf(String name) {
+        Sportart sportart = null;
+        for (Sportart s : SPORTARTEN) {
+            if (s.name.equals(name)) {
+                sportart = s;
+                break;
+            }
+        }
+        return sportart;
+    }
+
+    public Art getArt() {
         return art;
     }
-    
-    
-    
-    public static void main(String[] args){
+
+    public String name() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public static void main(String[] args) {
         System.out.println("Ausgabe von toString der enum SportNamen:");
-        for(Sportart s: Sportart.values()){
+        for (Sportart s : Sportart.values()) {
             System.out.println(s.name());
         }
     }
